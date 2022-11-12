77d1b50bb1c5f2812e4ffd5d0063988c81979ccd
Victoria Díaz <info.victoriadiaz@gmail.com>
2022-11-12 16:45:59 -0300

    3ra entrega

--- js/codigo.js
+++ js/codigo.js
@@ -1,9 +1,31 @@
 let productos = [
-    {id:1 , nombre: 'RemeraBasic', stock: 10, precio: 3500, imgUrl: './img2/remeraOversizeRoja.png'},
-    {id:2, nombre: 'RemeraBasic Negra', stock: 12, precio: 3500, imgUrl:'./img2/remeraOversizenNegra.png'},
-    {id:3, nombre: 'Buzo Oversize Black', stock: 9, precio: 9300, imgUrl: './img2/buzoOversizeNegro.jpeg'},
-    {id:4, nombre: 'Buzo Oversize  White', stock: 5, precio: 9300, imgUrl: './img2/buzoOversizeBlanco.jpeg'},
-    {id:5, nombre: 'Jean Tiro Alto', stock: 3, precio: 7500, imgUrl: './img2/pantalon.png'}
+    {id:1 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7565.PNG', texto: "loremmmmmm"},
+    {id:2 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7581.PNG', texto: "loremmmmmm"},
+    {id:3, nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7582.PNG', texto: "loremmmmmm"},
+    {id:4 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7559.PNG', texto: "loremmmmmm"},
+    {id:5 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7561.PNG', texto: "loremmmmmm"},
+    {id:6 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7563.PNG', texto: "loremmmmmm"},
+    {id:7 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7560.PNG', texto: "loremmmmmm"},
+    {id:8 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7562.PNG', texto: "loremmmmmm"},
+    {id:9 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7566.PNG', texto: "loremmmmmm"},
+    {id:10 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7567.PNG', texto: "loremmmmmm"},
+    {id:11 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7564.PNG', texto: "loremmmmmm"},
+    {id:12 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7568.PNG', texto: "loremmmmmm"},
+    {id:13 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7569.PNG', texto: "loremmmmmm"},
+    {id:14 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7570.PNG', texto: "loremmmmmm"},
+    {id:15 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7571.PNG', texto: "loremmmmmm"},
+    {id:16 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7575.PNG', texto: "loremmmmmm"},
+    {id:17 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7574.PNG', texto: "loremmmmmm"},
+    {id:18 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7573.PNG', texto: "loremmmmmm"},
+    {id:19 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7572.PNG', texto: "loremmmmmm"},
+    {id:20 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7579.PNG', texto: "loremmmmmm"},
+    {id:21 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7578.PNG', texto: "loremmmmmm"},
+    {id:22 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7577.PNG', texto: "loremmmmmm"},
+    {id:23, nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7576.PNG', texto: "loremmmmmm"},
+    {id:24 , nombre: '..', stock: 1, precio: 3500, imgUrl: './img/IMG_7580.PNG', texto: "loremmmmmm"},
+    
+   
+    
 ]
 
 
@@ -33,6 +55,7 @@ function renderizarProductos(productosFiltrados) {
         tarjetaProducto.className = 'producto'
         tarjetaProducto.innerHTML = `
     <h3 class="tituloProducto" >${producto.nombre}</h3>
+    <p class="textoProducto">${producto.texto}</p>
     <img class="imagenProducto" src=${producto.imgUrl} >
     <h4 class="precioProducto" >$${producto.precio}</h4>
     <button class="boton" id=${producto.id}>agregar carrito</button>
@@ -80,8 +103,8 @@ renderizarCarrito();
 function renderizarCarrito() {
     carrito.innerHTML = `
         <div class= "itemCarrito">
-        <p>nombre</p>
-        <p>precioUnidad</p>
+        <p>Articulo</p>
+        <p>precio</p>
         <p>unidades</p>
         <p>subtotal</p>
         </div>`
@@ -90,7 +113,7 @@ function renderizarCarrito() {
     for (const item of carritoGuardado) {
         total += item.subtotal
         carrito.innerHTML += `
-        <div class= "itemCarrito">
+        <div class= "itemCarrito2">
         <p>${item.nombre}</p>
         <p>${item.precioUnidad}</p>
         <p>${item.unidades}</p>
@@ -99,7 +122,7 @@ function renderizarCarrito() {
     }
 
     carrito.innerHTML += `
-    <div class= "itemCarrito">
+    <div class= "itemCarrito3">
         <h3>total: ${total}</h3>
         
         </div>
